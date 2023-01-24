859039682fafed7a3d188904e5777d5b9040549f
Jakub <meronentertainment@gmail.com>
2023-01-24 21:41:11 +0100

    Fixed & improved, tho still shitty, MotionPlanner

--- astable_invention/astable_invention/submodules/MotionPlanner.py
+++ astable_invention/astable_invention/submodules/MotionPlanner.py
@@ -16,11 +16,12 @@ class MotionPlanner:
 		(x,y) = (x+resolution/2,y+resolution/2)
 		angle = self.robot.Position.Angle
 		self.robot.Position.Update(x,y,angle)
+		self.robot.JobEngine.Schedule( Job.Absolute(x,y,0) )
 	def ObstacleReached(self, type):
 		(x,y) = (self.robot.Position.X, self.robot.Position.Y)
 		(dx,dy) = self.map.Facing(self.robot.Position.Angle)
-		pos = (x+dx, y+dy)
-		self.map.Set(pos, OBSTACLE)
+		self.map.Set((x,y), OBSTACLE)
+		self.map.Set((x+dx, y+dy), OBSTACLE)
 		self.robot.JobEngine.Clear()
 	def Tick(self):
 		pass
@@ -28,31 +29,7 @@ class MotionPlanner:
 		pos = (self.robot.Position.X, self.robot.Position.Y)
 		self.map.Set(pos, EMPTY)
 	def ObstacleAfterReflex(self, type):
-		(x,y) = (self.robot.Position.X, self.robot.Position.Y)
-		(dx,dy) = self.map.Facing(self.robot.Position.Angle)
-		pos = (x+dx, y+dy)
-		if self.map.Get(pos) == OBSTACLE:
-			resolution = Parameters.GridMapResolution
-			queue = deque( [(x,y)] )
-			visited = set( (x,y) )
-			parent = dict(); parent[ (x,y) ] = None
-			while queue:
-				curr = queue.popleft()
-				if self.map.Get(curr) == EMPTY:
-					path = []
-					while curr:
-						path.append(curr)
-						curr = parent[curr]
-					for (x,y) in path[::-1]:
-						self.robot.JobEngine.Schedule( Job.Absolute(x+resolution/2,y+resolution/2) )
-					return None
-				neighbours = [[resolution,0], [0,resolution], [-resolution,0], [0,-resolution]] 
-				for (dpos, weight, manhattan) in weighted:
-					(dx,dy) = dpos
-					if self.map.Get((x+dx,y+dy)) == OBSTACLE: continue
-					visited.add((x+dx,y+dy))
-					parent[(x+dx,y+dy)] = curr
-					queue.append( (x+dx , y+dy) )
+		pass
 	def ComputeWeight(self, dpos): 
 		(x, y) = (self.robot.Position.X, self.robot.Position.Y)
 		(dx, dy) = dpos
@@ -60,8 +37,16 @@ class MotionPlanner:
 		weight = 0
 		if facing == dpos: weight = weight + 1
 		if self.map.Get((x+dx, y+dy)) == UNEXPLORED: weight = weight + 3
-		if self.map.Get((x+dx, y+dy)) == OBSTACLE or self.map.Get((x+dx,y+dy)) == EMPTY: weight = 0
 		return weight
+	def StripInvalid(self, weighted):
+		output = []
+		(x, y) = (self.robot.Position.X, self.robot.Position.Y)
+		for (dpos, weight, manhattan) in weighted:
+			(dx, dy) = dpos
+			pos = (x+dx, y+dy)
+			if self.map.Get(pos) == OBSTACLE: continue
+			output.append([dpos, weight, manhattan])
+		return output
 	def PathToNearestUnexplored(self):
 		(x,y) = (self.robot.Position.X, self.robot.Position.Y)
 		resolution = Parameters.GridMapResolution
@@ -78,10 +63,10 @@ class MotionPlanner:
 				return path[::-1]
 			neighbours = [[resolution,0], [0,resolution], [-resolution,0], [0,-resolution]] 
 			weighted = [ [dpos, self.ComputeWeight(dpos), GridMap.ManhattanDistance(self.map.Center(), (x+dpos[0],y+dpos[1]))] for dpos in neighbours ]
-			weighted = [ i for i in weighted if i[1] > 0 ]
+			weighted = self.StripInvalid(weighted)
 			if weighted:
-				weighted = sorted(weighted, key=lambda x: -x[2])
-				weighted[0][1] = weighted[0][1] + 1
+				#weighted = sorted(weighted, key=lambda x: -x[2])
+				#weighted[0][1] = weighted[0][1] + 1
 				weighted = sorted(weighted, key= lambda x: -x[1])
 				for (dpos, weight, manhattan) in weighted:
 					(dx,dy) = dpos
@@ -94,7 +79,7 @@ class MotionPlanner:
 		resolution = Parameters.GridMapResolution
 		for pos in to_visit: 
 			(x,y) = pos
-			self.robot.JobEngine.Schedule( Job.Absolute(x+resolution/2,y+resolution/2) )
+			self.robot.JobEngine.Schedule( Job.Absolute(x,y) )
 		
 		
 			
